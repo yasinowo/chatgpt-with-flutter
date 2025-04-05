a2c614ffaa53c600726aba73855a688c6d90559f
yasinowo <zyzypwrmhmdyasyn@gmail.com>
2025-04-05 19:22:01 +0330

    defualt configur

--- lib/them/dark_mode.dart
+++ lib/them/dark_mode.dart
@@ -7,7 +7,7 @@ ThemeData darkMode = ThemeData(
       secondary: Color(0xff374151), // رنگ‌های فعال
       tertiary: Color(0xffffffff), // متن‌های روشن
       inversePrimary: Color(0xff1F2937), // متن‌های معمولی و دکمه‌های غیرفعال
-      primary: Color(0xff6B7280), // متن‌های قوی‌تر
+      primary: Color(0xff6B7280),
       shadow: Color(0xff030712), // سایه‌ها
       primaryContainer: Colors.black),
 );
